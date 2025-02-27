31c6ce099ffa1ca90e3c490513e49ee1017aef64
shalapaai <maimescum@gmail.com>
2025-02-27 12:52:00 +0300

    Lab4 First commit

---
+++ Lab4/Main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <img src="/8335_lk.png" width="300" height="300" alt="Login Image" weight>
+        <h1>ВОЙТИ</h1>
+        <p>Электронная почта</p>
+        <input type="text" name="Электронная почта" placeholder="Вводите почту">
+        <p>Пароль</p>
+        <input type="text" name="Пароль" placeholder="Ввод пароль">
+        <input type="submit">
+    </body>
+</html>
