f44a76ab1352973deb00232ba25b59e552abe9ec
shalapaai <maimescum@gmail.com>
2025-03-05 11:39:47 +0300

    Lab4 First Commit

--- Lab4/home.php
+++ Lab4/home.php
@@ -1,24 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="ru">
     <head>
         <title>Login</title>
     </head>
     <body>
         <img src="/c1ebddes-1920.jpg" width="30" height="30" alt="Avatar">
-        <span>Василий Гальперов</span>
-        <br>
+        <p>Василий Гальперов</p>
         <img src="/mountain.jpg" width="300" height="300" alt="Image">
-        <br>
         <img src="/like.jpg" width="30" height="30" alt="Like">
         <input type="submit" value="Like">
         <p>Я люблю горы</p>
         <p>2 часа назад</p>
 
         <img src="/putin.jpg" width="30" height="30" alt="Avatar">
-        <span>Вовка</span>
-        <br>
+        <p>Владимир Путин</p>
         <img src="/putin.jpg" width="300" height="300" alt="Image">
-        <br>
         <img src="/like.jpg" width="30" height="30" alt="Like">
         <input type="submit" value="Like">
         <p>Я не люблю горы</p>
