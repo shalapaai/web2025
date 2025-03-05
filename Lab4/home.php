1fd86a50878eb79eafc14beb46caba1461eb6ee2
shalapaai <maimescum@gmail.com>
2025-03-05 10:39:11 +0300

    Com

---
+++ Lab4/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <img src="/c1ebddes-1920.jpg" width="30" height="30" alt="Avatar">
+        <span>Василий Гальперов</span>
+        <br>
+        <img src="/mountain.jpg" width="300" height="300" alt="Image">
+        <br>
+        <img src="/like.jpg" width="30" height="30" alt="Like">
+        <input type="submit" value="Like">
+        <p>Я люблю горы</p>
+        <p>2 часа назад</p>
+
+        <img src="/putin.jpg" width="30" height="30" alt="Avatar">
+        <span>Вовка</span>
+        <br>
+        <img src="/putin.jpg" width="300" height="300" alt="Image">
+        <br>
+        <img src="/like.jpg" width="30" height="30" alt="Like">
+        <input type="submit" value="Like">
+        <p>Я не люблю горы</p>
+        <p>1 час назад</p>
+    </body>
+</html>
--- Lab4/login.php
+++ Lab4/login.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="ru">
     <head>
         <title>Login</title>
     </head>
     <body>
-        <img src="/8335_lk.png" width="300" height="300" alt="Login Image" weight>
+        <img src="/8335_lk.png" width="300" height="300" alt="Login Image">
         <h1>ВОЙТИ</h1>
         <p>Электронная почта</p>
         <input type="text" name="Электронная почта" placeholder="Вводите почту">
+        <p>Введите электропочту в формате *****@***.**</p>
         <p>Пароль</p>
         <input type="text" name="Пароль" placeholder="Ввод пароль">
         <input type="submit">
