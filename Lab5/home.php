157c8c59cce0cf1b8c8879fda35e55a680eefbda
shalapaai <maimescum@gmail.com>
2025-03-06 12:33:44 +0300

    Lab5 First Commit

---
+++ Lab5/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <img src="/c1ebddes-1920.jpg" width="30" height="30" alt="Avatar">
+        <p>Василий Гальперов</p>
+        <img src="/mountain.jpg" width="300" height="300" alt="Image">
+        <img src="/like.jpg" width="30" height="30" alt="Like">
+        <input type="submit" value="Like">
+        <p>Я люблю горы</p>
+        <p>2 часа назад</p>
+
+        <img src="/putin.jpg" width="30" height="30" alt="Avatar">
+        <p>Владимир Путин</p>
+        <img src="/putin.jpg" width="300" height="300" alt="Image">
+        <img src="/like.jpg" width="30" height="30" alt="Like">
+        <input type="submit" value="Like">
+        <p>Я не люблю горы</p>
+        <p>1 час назад</p>
+    </body>
+</html>
---
+++ Lab5/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <img src="/8335_lk.png" width="300" height="300" alt="Login Image">
+        <h1>ВОЙТИ</h1>
+        <p>Электронная почта</p>
+        <input type="text" name="Электронная почта" placeholder="Вводите почту">
+        <p>Введите электропочту в формате *****@***.**</p>
+        <p>Пароль</p>
+        <input type="text" name="Пароль" placeholder="Ввод пароль">
+        <input type="submit">
+    </body>
+</html>
